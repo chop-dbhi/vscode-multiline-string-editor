d8e6c235cadff067ed6fa56ddb5ae49e093c337f
Jacob Levernier <levernierj@email.chop.edu>
2020-01-06 07:23:21 -0500

    Forked stringified-json-editor by toiroakr and amended it to work with multiline strings generally.

--- README.md
+++ README.md
@@ -1,35 +1,36 @@
-<div align="center">
-  <div style="width: 160px">
+# Multiline String Editor
 
-  ![icon](./assets/icon.png)
-  </div>
-
-  # Stringified JSON Editor
-</div>
-
+Easily edit multiline strings that need to be encoded as single-line strings (for example, JSON strings that contain literal `\n` characters).
 
 ## Features
+
 Open new column for editing stringified JSON in JSON, and update JSON on save.
 
 ![how_to_use](./assets/how_to_use.gif)
 
 ## How to Use
+
+![caret_on_target](./assets/how_to_use.png)
+
 ### 1. Move caret on target string.
+
 ![caret_on_target](./assets/caret_on_target.png)
 
-### 2. Open command palette and select `Edit as stringified JSON`.
-![select_command](./assets/select_command.png)
+### 2. Open command palette and select `Edit as multiline string`.
 
-### 3. Edit JSON in opened `stringified.json`.
-|  before  |  after  |
-| :----: | :----: |
-|  ![edit_stringified_before](./assets/edit_stringified_before.png)  | ![edit_stringified_after](./assets/edit_stringified_after.png)  |
+### 3. Edit opened `stringified.txt`.
 
-### 4. Save JSON in opened `stringified.json`, then update original JSON.
-![save_stringified](./assets/save_stringified.png)
+## Developing
 
+1. Clone the repository, and install its dependencies:
 
-## Others
-[Issues](https://github.com/toiroakr/stringified-json-editor/issues)
+```
+git clone <this repository>
+cd <this directory>
+npm install
+code .
+```
 
-[Releases](https://github.com/toiroakr/stringified-json-editor/releases)
+2. Run `npm run compile`.
+3. Press `F5` to launch a development window.
+4. You can refresh the development window (from within the development window) with `Cmd + Shift + P` -> `Developer: Reload Window`.
--- src/extension.ts
+++ src/extension.ts
@@ -1,115 +1,141 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { MemFS } from './fileSystemProvider';
+import {MemFS} from './fileSystemProvider';
 
 const MEM_FS_SCHEMA = 'memfs_sje';
-const MEM_FS_FILE = `${MEM_FS_SCHEMA}:/stringified.json`;
+const MEM_FS_FILE = `${MEM_FS_SCHEMA}:/stringified.txt`;
 
 interface EditTarget {
-	range: vscode.Range;
-	body: string;
+  range: vscode.Range;
+  body: string;
 }
 
 interface EditContext {
-	editor?: vscode.TextEditor;
-	range?: vscode.Range;
+  editor?: vscode.TextEditor;
+  range?: vscode.Range;
 }
 
 function positionEquals(a: vscode.Position, b: vscode.Position) {
-	return a.line === b.line && a.character === b.character;
+  return a.line === b.line && a.character === b.character;
 }
 
 function getTarget(editor: vscode.TextEditor): EditTarget | null {
-	const document = editor.document;
-	const selection = editor.selection;
+  const document = editor.document;
+  const selection = editor.selection;
 
-	const line = document.lineAt(selection.start.line);
-	const reversedLine = line.text.split('').reverse().join('');
+  const line = document.lineAt(selection.start.line);
+  const reversedLine = line.text
+    .split('')
+    .reverse()
+    .join('');
 
-	if (positionEquals(selection.start, selection.end)) {
-		const regex = /"(.*?)"(?!\\)/g;
+  if (positionEquals(selection.start, selection.end)) {
+    const regex = /"(.*?)"(?!\\)/g;
 
-		let a;
-		while ((a = regex.exec(reversedLine)) != null) {
-			const end = line.text.length - a.index - 1;
-			const start = end - a[1].length;
+    let a;
+    while ((a = regex.exec(reversedLine)) != null) {
+      const end = line.text.length - a.index - 1;
+      const start = end - a[1].length;
 
-			if (selection.start.character < start - 1 || end + 1 < selection.start.character) {
-				console.log(a);
-				continue;
-			}
+      if (
+        selection.start.character < start - 1 ||
+        end + 1 < selection.start.character
+      ) {
+        console.log(a);
+        continue;
+      }
 
-			const body = line.text.substring(start, end);
-			const range = new vscode.Range(
-				new vscode.Position(selection.start.line, start),
-				new vscode.Position(selection.end.line, end)
-			);
-			return { body, range };
-		}
-	}
-	return null;
+      const body = line.text.substring(start, end);
+      const range = new vscode.Range(
+        new vscode.Position(selection.start.line, start),
+        new vscode.Position(selection.end.line, end)
+      );
+      return {body, range};
+    }
+  }
+  return null;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	const memFs = new MemFS();
-	const uri = vscode.Uri.parse(MEM_FS_FILE);
-	const editContext: EditContext = {};
+  const memFs = new MemFS();
+  const uri = vscode.Uri.parse(MEM_FS_FILE);
+  const editContext: EditContext = {};
 
-	context.subscriptions.push(vscode.workspace.registerFileSystemProvider(MEM_FS_SCHEMA, memFs, { isCaseSensitive: true }));
-	context.subscriptions.push(vscode.workspace.onDidSaveTextDocument((td) => {
-		if (!(MEM_FS_FILE === td.uri.toString() && editContext.editor && editContext.range)) {
-			return;
-		}
+  context.subscriptions.push(
+    vscode.workspace.registerFileSystemProvider(MEM_FS_SCHEMA, memFs, {
+      isCaseSensitive: true,
+    })
+  );
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(td => {
+      if (
+        !(
+          MEM_FS_FILE === td.uri.toString() &&
+          editContext.editor &&
+          editContext.range
+        )
+      ) {
+        return;
+      }
 
-		console.log('saved:', td.getText());
-		try {
-			const updated = JSON.stringify(JSON.parse(td.getText())).replace(/\\/g, '\\\\').replace(/"/g, '\\"');
-			console.log('updated:', updated);
+      console.log('saved:', td.getText());
+      try {
+        const updated = td.getText().replace(/\n/g, '\\n');
+        console.log('updated:', updated);
 
-			const { range } = editContext;
-			editContext.editor.edit(editBuilder => {
-				editBuilder.replace(range, updated);
-			});
-			vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-			memFs.delete(uri);
-		} catch (e) {
-			vscode.window.showErrorMessage(`Syntax Error: "${e.message}"`);
-		}
-	}));
+        const {range} = editContext;
+        editContext.editor.edit(editBuilder => {
+          editBuilder.replace(range, updated);
+        });
+        vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+        memFs.delete(uri);
+      } catch (e) {
+        vscode.window.showErrorMessage(`Syntax Error: "${e.message}"`);
+      }
+    })
+  );
 
-	const disposable = vscode.commands.registerCommand('extension.editStringified', function () {
-		// Get the active text editor
-		const editor = vscode.window.activeTextEditor;
-		editContext.editor = editor;
+  const disposable = vscode.commands.registerCommand(
+    'extension.editMultiline',
+    function() {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+      editContext.editor = editor;
 
-		if (editor == null) {
-			return;
-		}
+      if (editor == null) {
+        return;
+      }
 
-		if(MEM_FS_FILE === editor.document.uri.toString()) {
-			vscode.window.showErrorMessage('Cannot edit text in editing stringified JSON.');
-			return;
-		}
+      if (MEM_FS_FILE === editor.document.uri.toString()) {
+        vscode.window.showErrorMessage(
+          'Cannot edit text in editing stringified JSON.'
+        );
+        return;
+      }
 
-		const target = getTarget(editor);
-		if (target == null) {
-			vscode.window.showErrorMessage('Cannot get target string.');
-			return;
-		}
-		editContext.range = target.range;
+      const target = getTarget(editor);
+      if (target == null) {
+        vscode.window.showErrorMessage('Cannot get target string.');
+        return;
+      }
+      editContext.range = target.range;
 
-		let content;
-		try {
-			content = JSON.stringify(JSON.parse(target.body.replace(/\\"/g, '"').replace(/\\\\/g, '\\')), null, 4);
-		} catch (e) {
-			content = target.body.replace(/\\"/g, '"');
-		}
-		memFs.writeFile(uri, Buffer.from(content), { create: true, overwrite: true });
-		vscode.workspace.openTextDocument(uri).then(doc => {
-			vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
-		});
-	});
+      let content;
+      try {
+        content = target.body.replace(/\\n/g, '\n');
+      } catch (e) {
+        content = target.body.replace(/\\"/g, '"');
+      }
+      memFs.writeFile(uri, Buffer.from(content), {
+        create: true,
+        overwrite: true,
+      });
+      vscode.workspace.openTextDocument(uri).then(doc => {
+        vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside);
+      });
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
